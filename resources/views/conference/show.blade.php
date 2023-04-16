337b040f2ab381caaf25ea842f3c33c62e992524
Dovyd4s <vaitoska.dovydas@gmail.com>
2023-04-16 16:13:04 +0300

    Tweaks to show conference title on show view

--- resources/views/conference/show.blade.php
+++ resources/views/conference/show.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
-
+@section('title')
+    Conference: {{$conference['title']}}
+@endsection
 @section('content')
     <h1>{{$conference['title']}}</h1>
     <p>{{$conference['description']}}</p>
