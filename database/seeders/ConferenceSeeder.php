bb9f5c93ec4a94772d7d0e40b4662e28f261f1ed
Dovyd4s <vaitoska.dovydas@gmail.com>
2023-04-16 13:18:29 +0300

    create ArticlesSeeder

---
+++ database/seeders/ConferenceSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Conference;
+use Carbon\Carbon;
+use Faker\Provider\Lorem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ConferenceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        (new Conference())->insert([
+            [
+                'title' => Lorem::sentence(5),
+                'description' => Lorem::text(),
+                'dateOfConference' => Carbon::create('2023', '05', '01'),
+                'address' => "adresas x"
+            ],
+            [
+                'title' => Lorem::sentence(5),
+                'description' => Lorem::text(),
+                'dateOfConference' => Carbon::create('2023', '06', '20'),
+                'address' => "adresas 5"
+            ]
+        ]);
+    }
+}
