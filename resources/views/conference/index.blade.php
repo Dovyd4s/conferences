3568bb8a096a8cdc8d3302d43dc925ee56f145f2
Dovyd4s <vaitoska.dovydas@gmail.com>
2023-04-17 10:22:36 +0300

    Create Users & Authentication functionality

--- resources/views/conference/index.blade.php
+++ resources/views/conference/index.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.app')
 @section('title', 'Conferencez')
 @section('content')
-    <a href="{{route('conferences.create')}}"><button type="button">Create New Conference!</button> </a>
+    @auth
+        <a href="{{route('conferences.create')}}"><button type="button">Create New Conference!</button> </a>
+    @endauth
+
     @foreach($conferences as $conference)
         <a href="{{route('conferences.show', ['conference' => $conference['id']])}}">
             <h2>{{$conference['title']}} ({{$conference['dateOfConference']}})</h2>
         </a>
         <p>{{$conference['description']}}</p>
-        <a href="{{route('conferences.edit', ['conference'=>$conference['id']])}}"><button type="button">Edit!</button> </a>
-        <form action="{{route('conferences.destroy', ['conference' => $conference['id']])}}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Delete</button>
-        </form>
+
+        @auth
+            <a href="{{route('conferences.edit', ['conference'=>$conference['id']])}}"><button type="button">Edit!</button> </a>
+            <form action="{{route('conferences.destroy', ['conference' => $conference['id']])}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Delete</button>
+            </form>
+        @endauth
         <br>
     @endforeach
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,20 @@
 </head>
 <header>
     <a href="/">Home</a>
+    @guest
+        <a href="{{route('register')}}">Register</a>
+        <a href="{{route('login')}}">Login</a>
+    @else
+        <a href="{{ route('logout') }}"
+           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+    @endguest
+
 </header>
 <body>
 <div>
